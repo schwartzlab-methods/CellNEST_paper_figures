aa6cc931095c841a7a4514a83ea494187aa8896f
anne04 <anne.06.cse@gmail.com>
2024-02-01 16:35:02 -0500

    Update figure_5_LUAD_heatmaps.py

--- figure_5_LUAD_heatmaps.py
+++ figure_5_LUAD_heatmaps.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     parser.add_argument( '--data_from', type=str, required=True, help='Path to the dataset to read from. Space Ranger outs/ folder is preferred. Otherwise, provide the *.mtx file of the gene expression matrix.')
     parser.add_argument( '--filter_min_cell', type=int, default=1 , help='Minimum number of cells for gene filtering') 
     parser.add_argument( '--tissue_position_file', type=str, default='None', help='If your --data_from argument points to a *.mtx file instead of Space Ranger, then please provide the path to tissue position file.')
-
+    parser.add_argument( '--output_name', type=str, default='NEST_figures_output/', help='Output file name prefix according to user\'s choice')
     args = parser.parse_args()
     
     temp = sc.read_10x_mtx(args.data_from)
